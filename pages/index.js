14d25f5a38f6baa686b56078bc78dc03bbd9031a
Rafael Angeline <me@rafaelangeline.com>
2018-04-13 03:04:45 -0300

    Add prettier, jest and Travis integration

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+const Index = () => (
+	<div>
+		<h1>JS Standard Project</h1>
+		<p>Hello World!</p>
+
+		<style jsx>{`
+			h1 {
+				color: #333;
+				font-size: 32px;
+			}
+		`}</style>
+	</div>
+);
+
+export default Index;
